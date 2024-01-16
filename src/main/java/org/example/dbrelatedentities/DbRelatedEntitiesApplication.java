13b123b45ea361837d4df09fe09d0be6b35ae1b5
maroua <marouaamal2002@gmail.com>
2024-01-16 16:18:35 +0100

    DB-Related-Entities

---
+++ src/main/java/org/example/dbrelatedentities/DbRelatedEntitiesApplication.java
@@ -0,0 +1,13 @@
+package org.example.dbrelatedentities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbRelatedEntitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbRelatedEntitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/dbrelatedentities/DbRelatedEntitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dbrelatedentities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbRelatedEntitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
